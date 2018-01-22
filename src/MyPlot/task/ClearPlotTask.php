461908512d3de739f036b0818d7f808c78cfba17
jasonwynn10 <jasonwynn10@gmail.com>
2018-01-22 10:29:50 -0500

    Use old clearing method

--- src/MyPlot/task/ClearPlotTask.php
+++ src/MyPlot/task/ClearPlotTask.php
@@ -5,7 +5,6 @@ namespace MyPlot\task;
 use MyPlot\MyPlot;
 use MyPlot\Plot;
 use pocketmine\block\Block;
-use pocketmine\level\Level;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
 use pocketmine\scheduler\PluginTask;
@@ -40,44 +39,43 @@ class ClearPlotTask extends PluginTask {
 		$this->pos = new Vector3($this->plotBeginPos->x, 0, $this->plotBeginPos->z);
 		$this->plugin = $plugin;
 		$plugin->getLogger()->debug("Clear Task started at plot {$plot->X};{$plot->Z}");
-		$bb = $this->plugin->getPlotBB($this->plot);
-		foreach($this->plugin->getPlotChunks($this->plot) as $chunk) {
-			foreach($chunk->getEntities() as $entity) {
-				if($bb->isVectorInXZ($entity)) {
-					if(!$entity instanceof Player) {
-						$entity->close();
-					} else {
-						$this->plugin->teleportPlayerToPlot($entity, $this->plot);
-					}
-				}
-			}
-			foreach($chunk->getTiles() as $tile) {
-				if($bb->isVectorInXZ($tile)) {
-					$tile->close();
-				}
-			}
-		}
 	}
 
 	/**
 	 * @param int $currentTick
 	 */
 	public function onRun(int $currentTick) : void {
+		foreach($this->level->getEntities() as $entity) {
+			if(($plot = $this->plugin->getPlotByPosition($entity)) != null) {
+				if($plot->X === $this->plotBeginPos->x and $plot->Z === $this->plotBeginPos->z) {
+					if(!$entity instanceof Player) {
+						$entity->close();
+					}
+					else {
+						$this->plugin->teleportPlayerToPlot($entity, $plot);
+					}
+				}
+			}
+		}
 		$blocks = 0;
 		while($this->pos->x < $this->xMax) {
 			while($this->pos->z < $this->zMax) {
-				while($this->pos->y < Level::Y_MAX) {
+				while($this->pos->y < 128) {
 					if($this->pos->y === 0) {
 						$block = $this->bottomBlock;
-					}elseif($this->pos->y < $this->height) {
+					}
+					elseif($this->pos->y < $this->height) {
 						$block = $this->plotFillBlock;
-					}elseif($this->pos->y === $this->height) {
+					}
+					elseif($this->pos->y === $this->height) {
 						$block = $this->plotFloorBlock;
-					}else{
-						$block = Block::get(Block::AIR);
+					}
+					else {
+						$block = Block::get(0);
 					}
 					$this->level->setBlock($this->pos, $block, false, false);
-					if($blocks++ === $this->maxBlocksPerTick) {
+					$blocks++;
+					if($blocks === $this->maxBlocksPerTick) {
 						$this->getOwner()->getServer()->getScheduler()->scheduleDelayedTask($this, 1);
 						return;
 					}
@@ -88,6 +86,13 @@ class ClearPlotTask extends PluginTask {
 			}
 			$this->pos->z = $this->plotBeginPos->z;
 			$this->pos->x++;
+		}
+		foreach($this->level->getTiles() as $tile) {
+			if(($plot = $this->plugin->getPlotByPosition($tile)) != null) {
+				if($plot->X === $this->plotBeginPos->x and $plot->Z === $this->plotBeginPos->z) {
+					$tile->close();
+				}
+			}
 		}
 		$this->plugin->getLogger()->debug("Clear task completed at {$this->plotBeginPos->x};{$this->plotBeginPos->z}");
 	}
